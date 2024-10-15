e9e1325356a1d74680b598c553e6c5965a9ad790
Jagannath timma <jt@kloudfuse.com>
2024-10-15 13:33:25 -0700

    added comments
    Signed-off-by: Jagannath timma <jt@kloudfuse.com>

--- scripts/replication-factor/kafka_replication_increase.py
+++ scripts/replication-factor/kafka_replication_increase.py
@@ -2,6 +2,15 @@ import os
 import json
 import argparse
 
+"""
+Given a set of replicas - current_replicas, and desired replication_factor - rf,
+adds needed number of replicas for every patition in a topic.
+- keeps current replicas as is (no data movement)
+- ensures new replica is not a duplicate
+Eg:
+current_replicas = [100, 102], desired_rf = 3, num_brokers = 7, new_replicas = [103] so output will be [100, 102, 103]
+current_replicas = [100, 106], desired_rf = 3, num_brokers = 7, new_replicas = [101] so output will be [100, 106, 101]
+"""
 def reassign(args):
     topic_metadata = get_proposal(args.proposal_file)
     print(f"Current topic info: {topic_metadata}")
@@ -16,9 +25,8 @@ def reassign(args):
         # kafka replica ids start with 100
         new_replicas = []
         while len(new_replicas) < (args.rf - len(current_replicas)):
-            prev = last_replica
-            last_replica = 100 + ((last_replica - 100 + 1) % args.num_brokers)
-            if last_replica in current_replicas:
+            last_replica = 100 + ((last_replica - 100 + 1) % args.num_brokers) # find next replica to assign
+            if last_replica in current_replicas: # newly found replica may already be in current set
                 continue
             # print(f"State: {len(new_replicas)}, last: {prev}, need: {args.rf - len(current_replicas)}, added: {last_replica}")
             new_replicas.append(last_replica)
