8427a8f822cf2411af983334e6c6ed9f047acdba
Swapnil Kulkarni <swapnil@kloudfuse.com>
2025-03-19 21:31:30 +0530

    Added option for specifying optional namespace with kfuse as default

--- scripts/backupconfigdb.sh
+++ scripts/backupconfigdb.sh
@@ -2,7 +2,13 @@
 set -x
 
 S3_PATH=$1
-PGPW=`kubectl -n kfuse get secret kfuse-pg-credentials -o jsonpath="{.data.postgresql-password}" | base64 --decode ; echo`
+NAMESPACE=$2
+
+if [ -z "$NAMESPACE" ] ; then
+  NAMESPACE=kfuse
+fi
+
+PGPW=`kubectl -n "$NAMESPACE" get secret kfuse-pg-credentials -o jsonpath="{.data.postgresql-password}" | base64 --decode ; echo`
 
 if [ -z "$PGPW" ] ; then
   echo "Secret not found in the kfuse namespace"
@@ -10,12 +16,12 @@ if [ -z "$PGPW" ] ; then
 fi
 
 if [ -z "$S3_PATH" ] ; then
-  echo "Usage: ./backupconfigdb.sh s3path"
+  echo "Usage: ./backupconfigdb.sh s3path [namespace]"
   exit 1
 fi
 
 DBS=("alertsdb" "beffedb" "hydrationdb" "logsconfigdb" "rbacdb" "apmconfigdb")
 
 for DB in "${DBS[@]}"; do
-  kubectl exec -it kfuse-configdb-0 -- bash -c "PGPASSWORD=$PGPW pg_dump -U postgres -d $DB" | gzip | aws s3 cp - "$S3_PATH/$DB.sql.gz"
+  kubectl -n "$NAMESPACE exec -it kfuse-configdb-0 -- bash -c PGPASSWORD=$PGPW pg_dump -U postgres -d $DB" | gzip | aws s3 cp - "$S3_PATH/$DB.sql.gz"
 done
